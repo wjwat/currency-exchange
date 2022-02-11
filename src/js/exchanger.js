dad4aa169b2e2b30a3b276527f1023a7a6a34ade
Will W <wjwat@onionslice.org>
2022-02-11 14:06:46 -0600

    Finalize exchanger object & static method

--- src/js/exchanger.js
+++ src/js/exchanger.js
@@ -1,8 +1,25 @@
 export default class CurrencyExchanger {
   static async getRates(currencyCode) {
+    // pulled from https://www.exchangerate-api.com/docs/standard-requests
+    const errorResponses = {
+      "unsupported-code": "We do not currently support that currency code.",
+      "malformed-request": "The request provided is malformed, please check the provided URL.",
+      "invalid-key": "The provided API key is invalid.",
+      "inactive-account": "Your account is currently inactive. Please confirm your email address to activate your account.",
+      "quota-reached": "You have reached your quota of requests for the month. Check your account to see when you can make more requests."
+    };
+
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currencyCode}`);
-      return response;
+      const result = await response.json();
+
+      if (response.status === 200 && result.result === 'success') {
+        return result;
+      } else if (result.result === 'error' && result['error-type'] in errorResponses) {
+        throw new Error(`${result['error-type']}: ${errorResponses[result['error-type']]}`);
+      } else {
+        throw new Error(`${response.status}: ${response.statusText}`);
+      }
     } catch (error) {
       return error;
     }
