d651c8f1dab52449a5e227af572a11271c84a9be
Will W <wjwat@onionslice.org>
2022-02-11 12:41:20 -0600

    WIP & pivot towards filling in values in UI

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import $ from 'jquery';
 import './css/yacck.css';
 import './css/styles.css';
-import CurrencyExchanger from './js/exchanger';
+import CurrencyExchanger from './js/exchanger.js';
+import fillSelectOptions from './js/utils.js'
 
 const currencies = require('./js/currencies.json');
 
@@ -11,10 +12,14 @@ $('form').on('submit', (e) => {
     .then(resp => {
       console.log(resp);
       if (resp.status === 200) {
-        console.log('we here 1');
-        console.log(resp);
+        // call succesful completion
+        return resp.json();
       } else if (resp instanceof Error) {
+        // big ol' error time!
         console.error(resp.name, resp.message);
+      } else {
+        // fetch was successful but server responded with something other than a 200
+        console.log(resp);
       }
     });
 });
--- src/js/exchanger.js
+++ src/js/exchanger.js
@@ -2,7 +2,7 @@ export default class CurrencyExchanger {
   static async getRates(currencyCode) {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currencyCode}`);
-      return {[response.status]: response.json()};
+      return response;
     } catch (error) {
       return error;
     }
