d659e30bac6484db0a19b807c71ebfeab1eab4f6
Will W <wjwat@onionslice.org>
2022-02-11 10:39:00 -0600

    Add json file listing all available currencies

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import $ from 'jquery';
 import './css/yacck.css';
 import './css/styles.css';
 
+const currencies = require('./js/currencies.json');
+
+$(document).ready( () => {
+  console.log(currencies);
+});
