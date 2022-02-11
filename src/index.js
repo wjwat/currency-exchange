a24835c7bc17657f68111d5e88b7b9e28adf2008
Will W <wjwat@onionslice.org>
2022-02-11 14:40:16 -0600

    Display converted values & errors to user w/ classes applied

--- src/index.js
+++ src/index.js
@@ -2,24 +2,49 @@ import $ from 'jquery';
 import './css/yacck.css';
 import './css/styles.css';
 import CurrencyExchanger from './js/exchanger.js';
-import fillSelectOptions from './js/utils.js'
+import fillSelectOptions from './js/utils.js';
 
 const currencies = require('./js/currencies.json');
+const output = $('#output');
+
+function clearDisplay() {
+  output.text('');
+}
+
+function displayConvertedCurrency(resp, dollars, currency) {
+  const convertedAmount = resp['conversion_rates'][currency] * dollars;
+  const outString = `$${dollars} = ${convertedAmount}`;
+  output.attr('class', 'success');
+  output.html(outString);
+}
+
+function displayErrorMessage(error) {
+  output.attr('class', 'failure');
+  output.html(error.toString());
+}
 
 $('form').on('submit', (e) => {
   e.preventDefault();
-  CurrencyExchanger.getRates('ZZZ')
+  clearDisplay();
+
+  const currencyAmount = parseInt($('#currency-amount').val());
+  const currencyCode = $('#currencies option:selected').val();
+
+  if (!Number.isInteger(currencyAmount)) {
+    console.log('WRONG!');
+    return;
+  }
+
+  CurrencyExchanger.getRates('USD')
     .then(resp => {
-      console.log(resp);
-      if (resp.status === 200) {
-        // call succesful completion
-        return resp.json();
-      } else if (resp instanceof Error) {
-        // big ol' error time!
-        console.error(resp.name, resp.message);
-      } else {
-        // fetch was successful but server responded with something other than a 200
-        console.log(resp);
+      if (resp instanceof Error) {
+        throw resp;
       }
+      displayConvertedCurrency(resp, currencyAmount, currencyCode);
+    })
+    .catch(err => {
+      displayErrorMessage(err);
     });
 });
+
+$('#currencies').html(fillSelectOptions(Object.entries(currencies)));
