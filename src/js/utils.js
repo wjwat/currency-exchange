96a67259b04103c21eb0e699d5b544b9203f8f38
Will W <wjwat@onionslice.org>
2022-02-11 13:36:33 -0600

    Fix value of option

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,7 +1,7 @@
 export default function fillSelectOptions(options) {
   let retString = '';
   options.forEach(e => {
-    retString += `<option val="${e[0]}">${e[0]} / ${e[1].name} / ${e[1].country}</option>\n`;
+    retString += `<option value="${e[0]}">${e[0]} / ${e[1].name} / ${e[1].country}</option>\n`;
   });
   return retString;
 }
